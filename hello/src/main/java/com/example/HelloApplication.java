681d90fc2885909a849e8cd735ec64e138974a72
Jim Shingler <ShinglerJim@gmail.com>
2016-07-18 22:09:57 -0400

    Big Cleanup and fixed OAuth Token Relay - Big Thanks to Will Trans Example

--- hello/src/main/java/com/example/HelloApplication.java
+++ hello/src/main/java/com/example/HelloApplication.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +10,7 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -15,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
-import java.util.Date;
 import java.util.List;
 
 @SpringBootApplication
@@ -23,40 +25,36 @@ import java.util.List;
 @EnableResourceServer
 public class HelloApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
 }
 
 
 @RestController
 @RefreshScope
 class HelloController {
+    private static Logger log = LoggerFactory.getLogger(HelloController.class);
 
-	@Value("${hello.message}")
-	private String message = "NOTHING SET";
+    @Value("${hello.message}")
+    private String message = "NOTHING SET";
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
-	@RequestMapping("/hello")
-	public String hello(@RequestHeader(value="Authorization") String authorizationHeader,
-						Principal currentUser) {
+    @PreAuthorize("#oauth2.hasScope('health.events')")
+    @RequestMapping("/hello")
+    public String hello(@RequestHeader(value = "Authorization") String authorizationHeader,
+                        Principal currentUser) {
+        log.info("@@@@@ /hello " + currentUser.getName());
+        log.info("@@@@@ /hello " + authorizationHeader);
 
-//			MDC.put("productId", productId);
-//			LOG.info("ProductApi: User={}, Auth={}, called with productId={}", currentUser.getName(), authorizationHeader, productId);
+        return "HI THERE: " + message;
+    }
 
-		System.out.println("############################################ /hello " + new Date());
-		System.out.println("############################################ /hello " + currentUser.getName());
-		System.out.println("############################################ /hello " + authorizationHeader);
-
-		return "HI THERE: " + message ;
-	}
-
-	@Autowired
-	private DiscoveryClient discoveryClient;
-
-	@RequestMapping("/hello/service-instances/{applicationName}")
-	public List<ServiceInstance> serviceInstancesByApplicationName(
-			@PathVariable String applicationName) {
-		return this.discoveryClient.getInstances(applicationName);
-	}
+    @RequestMapping("/hello/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
 
 }
--- sayhello/src/main/java/com/example/SayhelloApplication.java
+++ sayhello/src/main/java/com/example/SayhelloApplication.java
@@ -2,35 +2,24 @@ package com.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
 import java.security.Principal;
-import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -41,12 +30,23 @@ public class SayhelloApplication {
     public static void main(String[] args) {
         SpringApplication.run(SayhelloApplication.class, args);
     }
+
+    @LoadBalanced
+    @Bean
+    public OAuth2RestTemplate loadBalancedTokenRelayingRestTemplate() {
+        return new TokenRelayingRestTemplate();
+    }
 }
 
-
-@RibbonClient(name = "hello", configuration = HelloConfiguration.class)
 @RestController
 class SayHelloController {
+    private static Logger log = LoggerFactory.getLogger(SayHelloController.class);
+
+    @Autowired
+    @Qualifier("loadBalancedTokenRelayingRestTemplate")
+    private OAuth2RestTemplate loadBalancedTokenRelayingRestTemplate;
+
+    private OAuth2RestTemplate tokenRelayingRestTemplate = new TokenRelayingRestTemplate();
 
 
     @Autowired
@@ -55,56 +55,31 @@ class SayHelloController {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
-    private LoadBalancerClient loadBalancerClient;
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-//    @LoadBalanced
-//    @Bean
-//    RestTemplate restTemplate(){
-//        return new RestTemplate();
-//    }
-
-    @Autowired
-    private OAuth2RestTemplate restTemplate;
-
     @PreAuthorize("#oauth2.hasScope('health.events')")
     @RequestMapping("/")
-    public String sayHello(@RequestHeader(value="Authorization") String authorizationHeader,
+    public String sayHello(@RequestHeader(value = "Authorization") String authorizationHeader,
                            Principal currentUser) throws JsonProcessingException {
         String greeting = "";
 
-        System.out.println("1. ############################################ /sayhello UserInfoURI " + resourceServerProperties.getUserInfoUri());
-        Map<?, ?> userInfoResponse = restTemplate.getForObject(resourceServerProperties.getUserInfoUri(),
+        log.info("##### /sayhello UserInfoURI " + resourceServerProperties.getUserInfoUri());
+        Map<?, ?> userInfoResponse = tokenRelayingRestTemplate.getForObject(resourceServerProperties.getUserInfoUri(),
                 Map.class);
 
-        System.out.println("############################################ /sayhello Date:         " + new Date());
-        System.out.println("############################################ /sayhello User:         " + currentUser.toString());
-        System.out.println("############################################ /sayhello Auth Header:  " + authorizationHeader);
-        System.out.println("############################################ /sayhello " + objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(userInfoResponse));
-//        System.out.println("############################################ /sayhello Access Token: " + restTemplate.getOAuth2ClientContext().getAccessToken().getValue());
+        log.info("##### /sayhello User:         " + currentUser.toString());
+        log.info("##### /sayhello Auth Header:  " + authorizationHeader);
+        log.info("##### /sayhello " + objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(userInfoResponse));
 
-        // http://www.todaysoftmag.com/article/1429/micro-service-discovery-using-netflix-eureka
-        // hello is the name of the service in
-        // Eureka, as well as of the Ribbon LoadBalancer
-        // which gets created automatically.
-        ServiceInstance instance = loadBalancerClient.choose("HELLO");
-        System.out.println("############################################ /sayhello instance" + instance);
+        greeting = loadBalancedTokenRelayingRestTemplate.getForObject("http://HELLO/hello", String.class);
+        log.info("##### Invoke Hello after ");
 
-        if (instance != null) {
-            // Invoke server, based on host and port.
-            // Example using RestTemplate.
-            URI helloUri = URI.create(String
-                    .format("http://%s:%s/hello/",
-                            instance.getHost(), instance.getPort()));
+        return "SayHello ->" + greeting;
+    }
 
-            System.out.println("############################################ Invoke Hello uri1: " + instance.getUri());
-            greeting = restTemplate.getForObject(helloUri, String.class);
-            System.out.println("############################################ Invoke Hello after ");
-        }
+    public void setOAuth2RestTemplate(OAuth2RestTemplate oAuth2RestTemplate) {
+        this.loadBalancedTokenRelayingRestTemplate = oAuth2RestTemplate;
+    }
 
-        return "SayHello(Ribbon) ->" + greeting;
+    public void setUserInfoRestTemplate(OAuth2RestTemplate userInfoRestTemplate) {
+        this.tokenRelayingRestTemplate = userInfoRestTemplate;
     }
 }
